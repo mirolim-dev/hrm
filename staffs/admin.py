7656253dcf6a3ce2592dc07cbd578d755ef55304
mrdjangodev <mirolimcoder@gmail.com>
2024-05-18 17:22:01 +0500

    custom form created for staff admin displation

--- staffs/admin.py
+++ staffs/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Staff, Ttj
+from .forms import StaffAdminForm
+
+class StaffAdmin(admin.ModelAdmin):
+    form = StaffAdminForm
+    list_display = ['first_name', 'last_name', 'phone', 'role', 'is_working', 'ttj_id']
+    search_fields = ['first_name', 'last_name', 'phone']
+    list_filter = ['role', 'is_working']
+
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(Ttj)
---
+++ staffs/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Staff
+
+class StaffAdminForm(forms.ModelForm):
+    class Meta:
+        model = Staff
+        fields = [
+            'first_name',
+            'last_name',
+            'phone',
+            'address',
+            'gender',
+            'passport',
+            'image',
+            'salary',
+            'is_working',
+            'ttj_id',
+            'role',
+        ]
