643b5bcb7b3f56989c0aea0a37e526cc4b473a30
mrdjangodev <mirolimcoder@gmail.com>
2024-05-18 17:23:24 +0500

    filtering staffs by ttj done

--- staffs/admin.py
+++ staffs/admin.py
@@ -7,7 +7,7 @@ class StaffAdmin(admin.ModelAdmin):
     form = StaffAdminForm
     list_display = ['first_name', 'last_name', 'phone', 'role', 'is_working', 'ttj_id']
     search_fields = ['first_name', 'last_name', 'phone']
-    list_filter = ['role', 'is_working']
+    list_filter = ['role', 'is_working', 'ttj_id']
 
 admin.site.register(Staff, StaffAdmin)
 admin.site.register(Ttj)
