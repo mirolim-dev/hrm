d427657a4dd0543a1605c1db9a1de36110a50e60
mrdjangodev <mirolimcoder@gmail.com>
2024-05-18 23:19:32 +0500

    Displaying StaffLeaving an Attandace on admin panel done

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import StaffLeaving, Attandance
 # Register your models here.
+
+class StaffLeavingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'staff_id', 'left_at']
+    search_fields = ['staff_id__first_name', 'staff_id__last_name', 'staff_id__ttj_id__name']
+    list_filter = ['staff_id__ttj_id', 'staff_id__gender', 'staff_id__role']
+admin.site.register(StaffLeaving, StaffLeavingAdmin)
+
+
+class AttandanceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'ttj_id', 'staff_id', 'tracked_at']
+    search_fields = ['ttj_id__name', 'staff_id__first_name', 'staff_id__last_name']
+    list_filter = ['ttj_id', 'staff_id__gender', 'staff_id__role']
+admin.site.register(Attandance, AttandanceAdmin)
