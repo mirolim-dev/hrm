0fc49590c32472b32fbc3636df1f834de3e588ec
mrdjangodev <mirolimcoder@gmail.com>
2024-05-19 19:34:17 +0500

    Displaying Attandance model on Django admin dashboard changed

--- actions/admin.py
+++ actions/admin.py
@@ -14,5 +14,5 @@ admin.site.register(StaffLeaving, StaffLeavingAdmin)
 class AttandanceAdmin(admin.ModelAdmin):
     list_display = ['id', 'ttj_id', 'staff_id', 'tracked_at']
     search_fields = ['ttj_id__name', 'staff_id__first_name', 'staff_id__last_name']
-    list_filter = ['ttj_id', 'staff_id__gender', 'staff_id__role', FilterAttandanceByTime]
+    list_filter = ['ttj_id', 'staff_id__role', FilterAttandanceByTime]
 admin.site.register(Attandance, AttandanceAdmin)
