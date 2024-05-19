6629b632a63f3dd1e03e1b691c11f4d06fb2c3c7
mrdjangodev <mirolimcoder@gmail.com>
2024-05-19 15:39:52 +0500

    apps' verbose name set

--- actions/apps.py
+++ actions/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class ActionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'actions'
+    verbose_name = 'Hodisalar'
     def ready(self) -> None:
         super().ready()
         from actions import signals
--- staffs/apps.py
+++ staffs/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StaffsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'staffs'
+    verbose_name = 'Xodimlar'
